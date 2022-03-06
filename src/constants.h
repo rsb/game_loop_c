9809cf74e4b7723d39f4979ddb5f036681f9fd99
Robert Scott-Buccleuch <60840+rsb@users.noreply.github.com>
2022-03-06 07:49:55 -0800

    [Create a Window] finished initialize_window function

---
+++ src/constants.h
@@ -0,0 +1,12 @@
+//
+// Created by Robert Buccleuch on 3/5/22.
+//
+
+#ifndef GAME_LOOP_CONSTANTS_H
+#define GAME_LOOP_CONSTANTS_H
+
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+#endif //GAME_LOOP_CONSTANTS_H
--- src/main.c
+++ src/main.c
@@ -1,7 +1,42 @@
 #include<stdio.h>
 #include <SDL.h>
+#include <stdbool.h>
+#include "constants.h"
+
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+bool initialize_window(void) {
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    fprintf(stderr, "Error initializing SDL \n");
+    return false;
+  }
+
+  window = SDL_CreateWindow(
+      NULL,
+      SDL_WINDOWPOS_CENTERED,
+      SDL_WINDOWPOS_CENTERED,
+      WINDOW_WIDTH,
+      WINDOW_HEIGHT,
+      SDL_WINDOW_BORDERLESS
+      );
+
+  if (! window) {
+    fprintf(stderr, "SDL_CreateWindow failed \n");
+    return false;
+  }
+
+  renderer = SDL_CreateRenderer(window, -1, 0);
+  if (! renderer) {
+    fprintf(stderr, "SDL_CreateRenderer failed \n");
+    return false;
+  }
+
+  return true;
+}
 
 int main() {
+  initialize_window();
   printf("Game is running... \n");
   return 0;
 }
